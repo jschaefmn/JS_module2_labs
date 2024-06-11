46f96eb8c1aad5726da48951a282a2766a396635
jakeschaef <jschaefmn@gmail.com>
2024-06-11 11:58:50 -0500

    finished future_value lab

--- future_value/future_value.js
+++ future_value/future_value.js
@@ -1,35 +1,45 @@
 "use strict";
+// this variable tells us if we should run the script again
+let shouldRepeat = "y";
 
-// get investment amount - loop until user enters a number
-let investment = 0;
+// main loop
 do {
-    investment = parseFloat(
+    // get investment amount - loop until user enters a number
+    let investment = 0;
+    do {
+        investment = parseFloat(
         prompt("Enter investment amount as xxxxx.xx", 10000));
-}
-while ( isNaN(investment) );
+    } while ( isNaN(investment) || investment <= 0);
+    // loop while investment IS NOT a number OR investment <= 0
+    
 
-// get interest rate - loop until user enters a number
-let rate = 0;
-do {
-    rate = parseFloat(prompt("Enter interest rate as xx.x", 7.5));
-}
-while ( isNaN(rate) );
+    // get interest rate - loop until user enters a number
+    let rate = 0;
+    do {
+        rate = parseFloat(prompt("Enter interest rate as xx.x", 7.5));
+    } while ( isNaN(rate) || rate <= 0 || rate >= 15);
+    // loop while rate IS NOT a number OR rate <= 0
+    // OR rate >= 15
 
-// get number of years - loop until user enters a number
-let years = 0;
-do {
-    years = parseInt(prompt("Enter number of years", 10));
-}
-while ( isNaN(years) );
+    // get number of years - loop until user enters a number
+    let years = 0;
+    do {
+        years = parseInt(prompt("Enter number of years", 10));
+    } while ( isNaN(years) || years <= 0);
+    // loop while years IS NOT a number OR years <= 0
 
-// calulate future value
-let futureValue = investment;
-for (let i = 1; i <= years; i++ ) {
-    futureValue = futureValue + (futureValue * rate / 100);
-}
+    // display the input values
+    document.write(`<h4>Investment amount = ${investment} Interest rate = ${rate} Years = ${years}</h4>`);
 
-// display results
-document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
-document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
-document.write(`<p><label>Years:</label> ${years}</p>`);
-document.write(`<p><label>Future Value:</label> ${futureValue.toFixed(2)}</p>`);
+    // calulate future value
+    let futureValue = investment;
+    for (let i = 1; i <= years; i++ ) {
+        const interest = futureValue * rate / 100;
+        futureValue = futureValue + (futureValue * rate / 100);
+        // display future values and interest
+        document.write(`<p>Year=${i} Interest=${interest.toFixed(2)} Value=${futureValue.toFixed(2)}</p>`);
+    }
+    // prompt user to see if program should run again
+    shouldRepeat = prompt("Run this again? (y/n)");
+
+} while(shouldRepeat == 'y' || shouldRepeat == 'Y');
